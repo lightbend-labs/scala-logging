138c6676421aaa812dc119b3d608628aae99cfdb
Ross A. Baker <ross@rossabaker.com>
2014-04-23 13:50:04 -0400

    Backport macros to simultaneously support Scala 2.10.x.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,7 +1,7 @@
 import sbt._
 
 object Version {
-  val scala        = "2.11.0"
+  val scala        = List("2.11.0", "2.10.4")
   val slf4j        = "1.7.7"
   val logback      = "1.1.2"
   val scalaTest    = "2.1.3"
@@ -9,7 +9,7 @@ object Version {
 }
 
 object Library {
-  val scalaReflect   = "org.scala-lang" % "scala-reflect"    % Version.scala
+  val scalaReflect   = "org.scala-lang" % "scala-reflect"
   val slf4jApi       = "org.slf4j"      %  "slf4j-api"       % Version.slf4j
   val logbackClassic = "ch.qos.logback" %  "logback-classic" % Version.logback
   val scalaTest      = "org.scalatest"  %% "scalatest"       % Version.scalaTest
@@ -23,8 +23,8 @@ object Dependencies {
   val scalaLoggingApi = List(
   )
 
-  val scalaLoggingSlf4j = List(
-    scalaReflect,
+  def scalaLoggingSlf4j(scalaVersion: String) = List(
+    scalaReflect % scalaVersion,
     slf4jApi,
     scalaTest % "test",
     mockitoAll % "test",
