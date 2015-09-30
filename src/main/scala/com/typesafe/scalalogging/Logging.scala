c75604bc02d7f769cd25a7ec8618f21a65fc5ec1
Jeffrey Aguilera <jeffrey@exabeam.com>
2015-09-29 20:21:35 -0700

    LazyLogging and StrictLogging now have @transient logger

--- src/main/scala/com/typesafe/scalalogging/Logging.scala
+++ src/main/scala/com/typesafe/scalalogging/Logging.scala
@@ -24,6 +24,7 @@ import org.slf4j.LoggerFactory
  */
 trait LazyLogging {
 
+  @transient
   protected lazy val logger: Logger =
     Logger(LoggerFactory.getLogger(getClass.getName))
 }
@@ -34,6 +35,7 @@ trait LazyLogging {
  */
 trait StrictLogging {
 
+  @transient
   protected val logger: Logger =
     Logger(LoggerFactory.getLogger(getClass.getName))
 }
