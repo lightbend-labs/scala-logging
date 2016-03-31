cbff2ef81577e6509673282598ac12400ec20b85
Mathias Bogaert <mathias.bogaert@gmail.com>
2016-03-31 11:45:57 +0100

    Logback 1.1.7, Scala 2.11.8 and SLF4J 1.7.20.

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -1,11 +1,11 @@
 import sbt._
 
 object Version {
-  val logback   = "1.1.5"
+  val logback   = "1.1.7"
   val mockito   = "1.9.5"
-  val scala     = "2.11.7"
+  val scala     = "2.11.8"
   val scalaTest = "2.2.6"
-  val slf4j     = "1.7.16"
+  val slf4j     = "1.7.20"
 }
 
 object Library {
