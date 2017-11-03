4bdbd5d6d54cb839841d944e66115f7e0ccfd093
Sean Sullivan <ssullivan@gilt.com>
2017-11-03 00:57:12 -0400

    Scala 2.12.2 -> 2.12.4

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -4,7 +4,7 @@ object Version {
   val logback   = "1.2.3"
   val mockito   = "1.10.19"
   val scala     = "2.11.11"
-  val crossScala = List(scala, "2.12.2", "2.13.0-M2")
+  val crossScala = List(scala, "2.12.4", "2.13.0-M2")
   val scalaTest = "3.0.4"
   val slf4j     = "1.7.25"
 }
