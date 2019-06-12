e5436ee8d6db2a5288ce6b6dedb46fc7346cddca
kenji yoshida <6b656e6a69@gmail.com>
2019-06-12 18:14:38 +0900

    Scala 2.13.0 (#171)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,10 +2,10 @@ import sbt._
 
 object Version {
   val logback   = "1.2.3"
-  val mockito   = "1.5.6"
+  val mockito   = "1.5.9"
   val scala     = "2.12.8"
-  val crossScala = List(scala, "2.11.12", "2.13.0-RC3")
-  val scalaTest = "3.0.8-RC5"
+  val crossScala = List(scala, "2.11.12", "2.13.0")
+  val scalaTest = "3.0.8"
   val slf4j     = "1.7.26"
 }
 
