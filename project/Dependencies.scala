06285eb49cba44bc66c2cf96c3acddabcac349d6
Scala Steward <me@scala-steward.org>
2019-11-01 00:45:47 +0100

    Update slf4j-api to 1.7.29

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Version {
   val scala     = "2.12.8"
   val crossScala = List(scala, "2.11.12", "2.13.0")
   val scalaTest = "3.0.8"
-  val slf4j     = "1.7.28"
+  val slf4j     = "1.7.29"
 }
 
 object Library {
