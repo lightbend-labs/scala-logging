9a74342975552f2baf806583acf7de50b87dd1c9
Seth Tisue <seth@tisue.net>
2021-04-07 15:13:35 -0700

    bump Scala versions (2.13.5, 3.0.0-RC2)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -2,8 +2,8 @@ import sbt._
 
 object Version {
   val logback   = "1.2.3"
-  val mockito   = "3.2.6.0"
-  val scalaTest = "3.2.6"
+  val mockito   = "3.2.7.0"
+  val scalaTest = "3.2.7"
   val slf4j     = "1.7.30"
 }
 
